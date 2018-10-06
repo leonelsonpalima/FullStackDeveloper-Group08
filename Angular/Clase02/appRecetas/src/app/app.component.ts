3fd5fa5364854c56512c934fbd80ed8529d560c6
Area51 <admin@Area51s-MacBook-Pro.local>
2018-10-06 15:34:48 -0500

    ngStyle, ngClass

--- Angular/Clase02/appRecetas/src/app/app.component.ts
+++ Angular/Clase02/appRecetas/src/app/app.component.ts
@@ -6,12 +6,14 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	recetas: Array<{ titulo: string, descripcion: string, palabrasClave, foto: string }>
+	recetas: Array<{ titulo: string, descripcion: string, palabrasClave, foto: string, rating: number, revisiones: number }>
 
 	titulo: string
 	descripcion: string
 	foto: string
 	palabrasClave: string
+	rating: number
+	revisiones: number
 
 	constructor() {
 		this.recetas = this.leerLS()
@@ -24,13 +26,17 @@ export class AppComponent {
 		const descripcion = this.descripcion
 		const palabrasClave = this.palabrasClave
 		const foto = this.foto
+		const rating = this.rating
+		const revisiones = this.revisiones
 
 		if (titulo != "" && descripcion != "" && foto != "" && palabrasClave != "") {
-			this.recetas.push({ titulo, descripcion, palabrasClave, foto })
+			this.recetas.push({ titulo, descripcion, palabrasClave, foto, rating, revisiones })
 			this.titulo = ""
 			this.descripcion = ""
 			this.foto = ""
 			this.palabrasClave = ""
+			this.rating = 0
+			this.revisiones = 0
 			this.escribirLS()
 		}
 	}
@@ -46,10 +52,26 @@ export class AppComponent {
 		localStorage.setItem("recetas", JSON.stringify(this.recetas))
 	}
 
-	leerLS(): Array<{ titulo: string, descripcion: string, palabrasClave, foto: string }> {
+	leerLS(): Array<{ titulo: string, descripcion: string, palabrasClave, foto: string, rating: number, revisiones: number }> {
 		if (localStorage.getItem("recetas")) {
 			return JSON.parse(localStorage.getItem("recetas"))
 		}
 		return []
+	}
+
+	obtenerColorLetra(receta) {
+		return receta.rating > 3 ? 'blue' : 'purple'
+	}
+
+	aplicaClaseInclinada(receta) {
+		return receta.revisiones > 10 ? true : false
+	}
+
+	aplicaClaseNormal(receta) {
+		return receta.revisiones <= 10 ? true : false
+	}
+
+	ngDoCheck() {
+		console.log("Algo pasó")
 	}
 }
